019a17e8940af77db6b1cb21a7998281ef4d3fdc
guilhermow <gui.crb0@gmail.com>
2020-06-07 08:44:58 -0300

    extracting addTaskDialog into a widget

---
+++ lib/widgets/add.task.dialog.dart
@@ -0,0 +1,60 @@
+import 'package:a7_guis_task_app/resources/resources.dart';
+import 'package:flutter/material.dart';
+
+class AddTaskDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20)
+      ),
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 10
+        ),
+        width: 300,
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: guisTaskAppTheme,
+                fontSize: 30,
+                fontWeight: FontWeight.w900
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              // controller: ,
+              autofocus: true,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            FlatButton(
+              color: guisTaskAppTheme,
+              padding: EdgeInsets.symmetric(
+                horizontal: 10,
+                vertical: 5
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10)
+              ),
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900
+                ),
+              ),
+              onPressed: (){},
+            )
+          ],
+        )
+      )
+    );
+  }
+}
--- lib/widgets/gui.dart
+++ lib/widgets/gui.dart
@@ -18,58 +18,7 @@ class Gui extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   SizedBox(height: 200,),
-                  Material(
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 30,
-                        vertical: 10
-                      ),
-                      width: 300,
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            'Add Task',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: guisTaskAppTheme,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w900
-                            ),
-                          ),
-                          SizedBox(height: 10),
-                          TextField(
-                            // controller: ,
-                            autofocus: true,
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          FlatButton(
-                            color: guisTaskAppTheme,
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 5
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)
-                            ),
-                            child: Text(
-                              'Add',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.w900
-                              ),
-                            ),
-                            onPressed: (){},
-                          )
-                        ],
-                      )
-                    )
-                  )
+                  AddTaskDialog()
                 ],
               )
             );
@@ -79,3 +28,4 @@ class Gui extends StatelessWidget {
     );
   }
 }
+
